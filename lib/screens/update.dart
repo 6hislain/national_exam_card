dd567c26a95d0d50bf72408143ce3d7571b20cee
6hislain <ghislainmike80@gmail.com>
2024-03-17 15:12:13 +0200

    visible title

--- lib/screens/update.dart
+++ lib/screens/update.dart
@@ -1,48 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:national_exam_card/utils/provider.dart';
 
 import '../components/my_card.dart';
+
 import '../schemas/application.dart';
 import '../schemas/mark.dart';
 import '../schemas/paper.dart';
 import '../schemas/notification.dart';
 import '../schemas/calendar_event.dart';
+
 import '../utils/isar_service.dart';
 
-class Update extends StatefulWidget {
+class Update extends ConsumerStatefulWidget {
   const Update({super.key});
 
   @override
-  State<Update> createState() => _UpdateState();
+  ConsumerState<Update> createState() => _UpdateState();
 }
 
-class _UpdateState extends State<Update> {
+class _UpdateState extends ConsumerState<Update> {
   IsarService db = IsarService();
-  List<Application> _applications = [];
-  List<Notifications> _notifications = [];
   List<Mark> _marks = [];
   List<Paper> _papers = [];
   List<CalendarEvent> _events = [];
-
-  Future<void> fetchDataFromDatabase() async {
-    var marks = await db.getMarks();
-    var papers = await db.getPapers();
-    var events = await db.getCalendarEvents();
-    var applications = await db.getApplications();
-    var notifications = await db.getNotifications();
-
-    setState(() {
-      _marks = marks;
-      _papers = papers;
-      _events = events;
-      _applications = applications;
-      _notifications = notifications;
-    });
-  }
+  List<Application> _applications = [];
+  List<Notifications> _notifications = [];
 
   @override
   void initState() {
     super.initState();
-    fetchDataFromDatabase();
+    _marks = ref.read(markStateProvider);
+    _papers = ref.read(paperStateProvider);
+    _events = ref.read(eventStateProvider);
+    _applications = ref.read(applicationStateProvider);
+    _applications = ref.read(applicationStateProvider);
+    _notifications = ref.read(notificationStateProvider);
   }
 
   @override
@@ -51,16 +46,19 @@ class _UpdateState extends State<Update> {
     return Container(
         padding: EdgeInsets.all(5),
         child: Wrap(runSpacing: 5, alignment: WrapAlignment.center, children: [
-          SizedBox(
-              height: 30,
-              width: screenSize.width * 0.9,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Student Exam Card Application',
-                        style: Theme.of(context).textTheme.bodyLarge)
-                  ])),
+          Visibility(
+            visible: _applications.isNotEmpty,
+            child: SizedBox(
+                height: 30,
+                width: screenSize.width * 0.9,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Student Exam Card Application',
+                          style: Theme.of(context).textTheme.bodyLarge)
+                    ])),
+          ),
           Container(
               width: screenSize.width / 0.9,
               child: MyCard(title: "Simon", subtitle: '...')),
@@ -70,16 +68,19 @@ class _UpdateState extends State<Update> {
           Container(
               width: screenSize.width / 0.9,
               child: MyCard(title: "Elene", subtitle: '...')),
-          SizedBox(
-              height: 30,
-              width: screenSize.width * 0.9,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Calendar Events',
-                        style: Theme.of(context).textTheme.bodyLarge)
-                  ])),
+          Visibility(
+            visible: _events.isNotEmpty,
+            child: SizedBox(
+                height: 30,
+                width: screenSize.width * 0.9,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Calendar Events',
+                          style: Theme.of(context).textTheme.bodyLarge)
+                    ])),
+          ),
           Container(
               width: screenSize.width / 2.1,
               child: MyCard(title: "Exam Day", subtitle: '...')),
@@ -89,30 +90,37 @@ class _UpdateState extends State<Update> {
           Container(
               width: screenSize.width / 2.1,
               child: MyCard(title: "Math Contest", subtitle: '...')),
-          SizedBox(
-              height: 30,
-              width: screenSize.width * 0.9,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Marks', style: Theme.of(context).textTheme.bodyLarge)
-                  ])),
+          Visibility(
+            visible: _marks.isNotEmpty,
+            child: SizedBox(
+                height: 30,
+                width: screenSize.width * 0.9,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Marks',
+                          style: Theme.of(context).textTheme.bodyLarge)
+                    ])),
+          ),
           for (var mark in _marks)
             Container(
               width: screenSize.width / 2.1,
               child: MyCard(title: '${mark.marks}', subtitle: '${mark.year}'),
             ),
-          SizedBox(
-              height: 30,
-              width: screenSize.width * 0.9,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Past Papers',
-                        style: Theme.of(context).textTheme.bodyLarge)
-                  ])),
+          Visibility(
+            visible: _papers.isNotEmpty,
+            child: SizedBox(
+                height: 30,
+                width: screenSize.width * 0.9,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Past Papers',
+                          style: Theme.of(context).textTheme.bodyLarge)
+                    ])),
+          ),
           Container(
               width: screenSize.width / 2.1,
               child: MyCard(title: "English 2022", subtitle: '...')),
@@ -122,16 +130,19 @@ class _UpdateState extends State<Update> {
           Container(
               width: screenSize.width / 2.1,
               child: MyCard(title: "Chemistry 2022", subtitle: '...')),
-          SizedBox(
-              height: 30,
-              width: screenSize.width * 0.9,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Notifications',
-                        style: Theme.of(context).textTheme.bodyLarge)
-                  ])),
+          Visibility(
+            visible: _notifications.isNotEmpty,
+            child: SizedBox(
+                height: 30,
+                width: screenSize.width * 0.9,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Notifications',
+                          style: Theme.of(context).textTheme.bodyLarge)
+                    ])),
+          ),
           for (var application in _applications)
             Container(
               width: screenSize.width / 2.1,
